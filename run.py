9a7e131f742c716a600f20708c20896a288993fa
MartinFortuna <martinperricault@gmail.com>
2022-12-04 23:16:20 +0000

    Add csv file and import libraries

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#############################################
+# Census: Ireland's population 2011 to 2016
+# Data Source:
+# https://data.gov.ie/dataset/e2022-population-2011-to-2016/resource/ff12fd4b-63a7-48e3-9c8b-a8f309f78cfa
+#############################################
+
+# Import Libraries
+import pandas as pd
+import os
+from datetime import datetime
